f9e5012d1dc8ddf9631f7a283db0f8210acbfb05
ADS\yli425 <yuan.li@aexp.com>
2019-01-01 15:05:15 -0700

    master/detail component

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
