6f07bc358c3b3940d27438e3996424eeac944601
ADS\yli425 <yuan.li@aexp.com>
2019-01-01 16:36:49 -0700

    create the MessageService

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -4,12 +4,14 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipesComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- UI/src/app/recipe.service.ts
+++ UI/src/app/recipe.service.ts
@@ -1,14 +1,17 @@
+import { MessageService } from './message.service';
 import { Recipe } from './recipe';
 import { RECIPES } from './mock-recipes';
 import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   getRecipes(): Observable<Recipe[]> {
+    this.messageService.add('RecipeService: fetched recipes');
     return of(RECIPES);
   }
 }
